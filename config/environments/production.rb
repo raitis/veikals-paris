2d1fea8bd0b64030ed12451afdbf1abdbf584025
raitis <raitis.medis@gmail.com>
2014-02-12 01:23:55 +0200

    Fix bucket configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,9 +68,9 @@ Website::Application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['VEIKALS-PARIS'],
-    :access_key_id => ENV['AKIAI4RCHL4Q5GJRK6GQ'],
-    :secret_access_key => ENV['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']
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 
